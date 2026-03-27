073d1d475516e71a4cba6857adf886655fc865b0
Adith Gunaseelan <54245667+AdithNG@users.noreply.github.com>
2026-03-27 09:35:57 -0700

    Revamp portfolio UI with modern styling and animations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,28 @@
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
+const navItems = [
+  { to: '/', label: 'Home', end: true },
+  { to: '/programming', label: 'Programming Portfolio' },
+  { to: '/games', label: 'Game Development Portfolio' },
+  { to: '/about', label: 'More About Me' },
+];
+
 function Navbar() {
   return (
     <nav className="navbar">
       <ul>
-        <li><NavLink to="/" end activeClassName="active">Home</NavLink></li>
-        <li><NavLink to="/programming" activeClassName="active">Programming Portfolio</NavLink></li>
-        <li><NavLink to="/games" activeClassName="active">Game Development Portfolio</NavLink></li>
-        <li><NavLink to="/about" activeClassName="active">More About Me</NavLink></li>
+        {navItems.map((item) => (
+          <li key={item.to}>
+            <NavLink
+              to={item.to}
+              end={item.end}
+              className={({ isActive }) => (isActive ? 'active' : undefined)}
+            >
+              {item.label}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   );
